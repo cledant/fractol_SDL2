8ec5da9dc3bc7188321c25a0f2430c5ee2bc2437
cledant <cledant@student.42.fr>
2016-12-19 00:52:53 +0100

    Updated Mandelbrot and Burningship calculations

--- Fractol_SDL2/fractol_mandelbrot.c
+++ Fractol_SDL2/fractol_mandelbrot.c
@@ -14,31 +14,43 @@
 
 void		fractol_mandelbrot(t_env *e)
 {
-	int	i = 0;
-	int	j = 0;
+	int		i = 0;
+	int		j = 0;
 	size_t	cur_it = 0;
-	char	draw = 0;
-	double	cpy_init_val[2];
-	const double z_r_init = 0;
-	const double z_i_init = 0;
+	double	z_r = 0;
+	double	z_i = 0;
+	double	res = 0;
+	double	tmp_r = 0;
+	double	tmp_i = 0;
+	double	sqrt_tmp_x = 0;
+	double	sqrt_tmp_y = 0;
 
-	cpy_init_val[0] = z_r_init;
-	cpy_init_val[1] = z_i_init;
 	while (j < e->var.win_height)
 	{
 		while (i < e->var.win_width)
 		{
-			while ((draw = fractol_calc_mb(&cpy_init_val[0], &cpy_init_val[1],
-					(e->var.width_min + (e->var.width_pitch * i)), (e->var.height_max -
-						(e->var.height_pitch * j))) == 0) && cur_it < e->var.iter)
+			while (cur_it < e->var.iter)
+			{
+				tmp_r = z_r;
+				tmp_i = z_i;
+				sqrt_tmp_x = tmp_r * tmp_r;
+				sqrt_tmp_y = tmp_i * tmp_i;
+				z_r = (sqrt_tmp_x - sqrt_tmp_y + e->var.width_min +
+					(e->var.width_pitch * i));
+				z_i = tmp_r * tmp_i;
+				z_i += z_i;
+				z_i += (e->var.height_max - (e->var.height_pitch * j));
+				if ((res = (z_r * z_r) + (z_i * z_i)) > 4)
+					break ;
 				cur_it++;
-			if (draw == 0)
+			}
+			if (res > 4)
 				fractol_color_pixel(&(e->fractal), e->var.color, e->var.win_width,
 					i, j, cur_it);
-			cpy_init_val[0] = z_r_init;
-			cpy_init_val[1] = z_i_init;
+			z_r = 0;
+			z_i = 0;
+			res = 0;
 			cur_it = 0;
-			draw = 0;
 			i++;
 		}
 		i = 0;
